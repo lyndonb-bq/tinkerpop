ed0ccefab8be08b76e6ecd0e2486e6453618d112
Rithin <rithinkumar13@gmail.com>
2022-04-28 13:16:24 -0700

    Maven Structure, Test in main function.

---
+++ gremlin-go/driver/perf-tests/src/main/java/PerformanceTest.java
@@ -0,0 +1,195 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// CAUTION!!!
+// This file is intended for generating Performance test results only.
+// It is not to be used in a production setting.
+// It is being made available as a reference to the test results for
+// anyone that wants see the code used to obtain the performance metrics.
+
+import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.Result;
+import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
+
+public class PerformanceTest {
+    public static void main(String[] args) {
+        executePerformanceTests();
+    }
+    static final int SAMPLE_SIZE = 11;
+    static final Cluster cluster = Cluster.build("localhost").port(8182).create();
+    static final DriverRemoteConnection connection = DriverRemoteConnection.using(cluster, "ggrateful");
+    static final GraphTraversalSource g = traversal().withRemote(connection);
+    static final int VALUEMAP_REPEATS = 50;
+
+    public static String[] getArgs(final int repeats) {
+        final String[] args = new String[repeats];
+        for (int i = 0; i < repeats; i++) {
+            args[i] = Integer.toString(i);
+        }
+        return args;
+    }
+
+    public static GraphTraversal<?, ?> getProjectTraversal(final GraphTraversalSource g, final int repeats, final String[] args) {
+        GraphTraversal<?, ?> traversal = g.V().project(args[0], Arrays.copyOfRange(args, 1, repeats));
+        for (int i = 0; i < repeats; i++) {
+            traversal = traversal.by(__.valueMap(true));
+        }
+        return traversal;
+    }
+
+    public static void executePerformanceTests() {
+        System.out.println("~~~~~~~ PERFORMANCE TESTS STARTED ~~~~~~~\"");
+        System.out.println("~~~~~~~ RUNNING ONE ITEM PERFORMANCE TEST ~~~~~~~");
+        executeGetNextPerformanceTest();
+        System.out.println("~~~~~~~ RUNNING LIST PERFORMANCE TEST ~~~~~~~");
+        executeGetToListPerformanceTest();
+        System.out.println("~~~~~~~ RUNNING TRANSFER PERFORMANCE TEST ~~~~~~~");
+        executeTransferPerformanceTest();
+        System.out.println("~~~~~~~ PERFORMANCE TESTS COMPLETE ~~~~~~~\"");
+    }
+
+    public static TimingData executeGetNextPerformanceTest() {
+        final List<Duration> durations = new ArrayList<>();
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            try {
+                final String[] args = getArgs(VALUEMAP_REPEATS);
+                final Instant starts = Instant.now();
+                // execute(timed)
+                final Object obj = getProjectTraversal(g, VALUEMAP_REPEATS, args).next();
+                final Instant ends = Instant.now();
+                durations.add(Duration.between(starts, ends));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        TimingData data = getTimingDataFromDurationList(durations);
+        System.out.println(data.toStringMillis("One Item"));
+        return data;
+    }
+
+    public static TimingData executeGetToListPerformanceTest() {
+        final List<Duration> durations = new ArrayList<>();
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            try {
+                final String[] args = getArgs(VALUEMAP_REPEATS);
+                final Instant starts = Instant.now();
+                // execute(timed)
+                final List<?> list = getProjectTraversal(g, VALUEMAP_REPEATS, args).toList();
+                final Instant ends = Instant.now();
+                durations.add(Duration.between(starts, ends));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        TimingData data = getTimingDataFromDurationList(durations);
+        System.out.println(data.toStringMillis("List"));
+        return data;
+    }
+
+    public static TimingData executeTransferPerformanceTest() {
+        final List<Duration> durations = new ArrayList<>();
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            try {
+                final String[] args = getArgs(VALUEMAP_REPEATS);
+                // execute(not timed)
+                final List<?> resultSet = getProjectTraversal(g, VALUEMAP_REPEATS, args).toList();
+                // retrieve(timed)
+                final Instant starts = Instant.now();
+                for (Object obj:resultSet) {
+                    Result result = (Result) obj;
+                    Object newObj = result.getObject();
+                }
+                final Instant ends = Instant.now();
+                durations.add(Duration.between(starts, ends));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        TimingData data = getTimingDataFromDurationList(durations);
+        System.out.println(data.toStringMillis("Transfer"));
+        return data;
+    }
+
+    private static TimingData getTimingDataFromDurationList(final List<Duration> durations) {;
+        Collections.sort(durations);
+        durations.remove(durations.size()-1);
+        return new TimingData(
+                durations.stream().reduce(Duration.ZERO, Duration::plus).dividedBy(SAMPLE_SIZE),
+                durations.get(durations.size() / 2),
+                durations.get((int)(durations.size() * 0.90)),
+                durations.get((int)(durations.size() * 0.95)),
+                durations.get(0),
+                durations.get(durations.size() - 1)
+        );
+    }
+}
+
+class TimingData {
+    final Duration AVG;
+    final Duration MEDIAN;
+    final Duration PERCENTILE_90;
+    final Duration PERCENTILE_95;
+    final Duration MIN;
+    final Duration MAX;
+
+    TimingData(Duration avg, Duration median, Duration percentile_90, Duration percentile_95, Duration min, Duration max) {
+        AVG = avg;
+        MEDIAN = median;
+        PERCENTILE_90 = percentile_90;
+        PERCENTILE_95 = percentile_95;
+        MIN = min;
+        MAX = max;
+    }
+
+    @Override
+    public String toString() {
+        return toStringMillis("unknown");
+    }
+
+    public String toStringMillis(final String testType) {
+        return "Test Type: " + testType + "\n"+
+                "\tAVG=" + AVG.toMillis() + "ms \n" +
+                "\tMEDIAN=" + MEDIAN.toMillis() + "ms \n" +
+                "\tPERCENTILE_90=" + PERCENTILE_90.toMillis() + "ms \n" +
+                "\tPERCENTILE_95=" + PERCENTILE_95.toMillis() + "ms \n" +
+                "\tMIN=" + MIN.toMillis() + "ms \n" +
+                "\tMAX=" + MAX.toMillis() + "ms \n";
+    }
+
+    public String toStringNanos(final String testType) {
+        return "Test Type " + testType + "\n"+
+                "\tAVG=" + AVG.toNanos() + "ms \n" +
+                "\tMEDIAN=" + MEDIAN.toNanos() + "ms \n" +
+                "\tPERCENTILE_90=" + PERCENTILE_90.toNanos() + "ms \n" +
+                "\tPERCENTILE_95=" + PERCENTILE_95.toNanos() + "ms \n" +
+                "\tMIN=" + MIN.toNanos() + "ms \n" +
+                "\tMAX=" + MAX.toNanos() + "ms \n";
+    }
+}
