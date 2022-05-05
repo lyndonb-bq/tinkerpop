cbfc5c5777deddef47ca8ff6654c777dc590edff
Lyndon Bauto <lyndonb@bitquilltech.com>
2022-05-05 15:35:54 -0700

    initial pooling updates

--- gremlin-go/driver/performance-tests/java-test/src/main/java/PerformanceTest.java
+++ gremlin-go/driver/performance-tests/java-test/src/main/java/PerformanceTest.java
@@ -17,16 +17,12 @@
  * under the License.
  */
 
-// DO NOT USE - THIS WILL BE DELETED
-// This file is intended for generating Performance test results only.
-// It is not to be used in a production setting.
-// It is being made available as a reference to the test results for
-// anyone that wants see the code used to obtain the performance metrics.
-
-package org.apache.tinkerpop.gremlin.golang.performance.comparison;
-
+import org.antlr.v4.misc.Graph;
+import org.apache.tinkerpop.gremlin.driver.Client;
 import org.apache.tinkerpop.gremlin.driver.Cluster;
+import org.apache.tinkerpop.gremlin.driver.ResultSet;
 import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection;
+import org.apache.tinkerpop.gremlin.process.remote.traversal.RemoteTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
@@ -36,22 +32,63 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal;
 
-public class PerformanceTest {
-    static final int SAMPLE_SIZE = 21;
-    static final int VALUE_MAP_REPEATS = 500;
-    static final Cluster cluster = Cluster.build("172.31.24.17").port(45940).maxContentLength(300 * 1024 *1024).create();
-    static final DriverRemoteConnection connection = DriverRemoteConnection.using(cluster, "ggrateful");
-    static final GraphTraversalSource g = traversal().withRemote(connection);
+/**
+ * DO NOT USE - THIS WILL BE DELETED
+ * This file is intended for generating Performance test results only.
+ * It is not to be used in a production setting.
+ * It is being made available as a reference to the test results for
+ * anyone that wants see the code used to obtain the performance metrics.
+ */
+class PerformanceTest {
+    private static final int SAMPLE_SIZE = 21;
+    private static final int VALUE_MAP_REPEATS = 500;
+    private static final String HOST = "localhost";
+    private static final int PORT = 45940;
+    private static final int POOLING_CONTENT_LENGTH = 25 * 1024 * 1024;
+    private static final int MAX_CONTENT_LENGTH = 300 * 1024 * 1024;
+    private static final String REMOTE_TRAVERSAL_SOURCE = "ggrateful";
+    private static final int POOLING_TRAVERSAL_SIZE = 10;
+    private static final List<Integer> POOL_SIZE = new ArrayList<>();
+    private static final List<Integer> POOL_QUERY_COUNT = new ArrayList<>();
+    static {
+        // POOL_SIZE.add(1);
+        // POOL_SIZE.add(2);
+        POOL_SIZE.add(4);
+        POOL_SIZE.add(8);
+        //POOL_QUERY_COUNT.add(50);
+        POOL_QUERY_COUNT.add(100);
+        // POOL_QUERY_COUNT.add(250);
+        // POOL_QUERY_COUNT.add(500);
+    }
 
     public static void main(String[] args) {
         executePerformanceTests();
         System.exit(0);
     }
 
-    public static String[] getArgs(final int repeats) {
+    private static DriverRemoteConnection getDriverRemoteConnection(final Integer poolSize) {
+        return DriverRemoteConnection.using(getCluster(poolSize), REMOTE_TRAVERSAL_SOURCE);
+    }
+
+    private static Cluster getCluster(final Integer poolSize) {
+
+        return (poolSize == null) ? Cluster.build(HOST).port(PORT).maxContentLength(MAX_CONTENT_LENGTH).create()
+                :Cluster.build(HOST).port(PORT).maxContentLength(POOLING_CONTENT_LENGTH).maxConnectionPoolSize(poolSize).create();
+    }
+
+    private static Client getClient(final Cluster cluster) {
+        return cluster.connect().alias(REMOTE_TRAVERSAL_SOURCE);
+    }
+
+    private static GraphTraversalSource getGraphTraversalSource(final DriverRemoteConnection connection) {
+        return traversal().withRemote(connection);
+    }
+
+    private static String[] getArgs(final int repeats) {
         final String[] args = new String[repeats];
         for (int i = 0; i < repeats; i++) {
             args[i] = Integer.toString(i);
@@ -59,7 +96,8 @@ public class PerformanceTest {
         return args;
     }
 
-    public static GraphTraversal<?, ?> getProjectTraversal(final GraphTraversalSource g, final int repeats, final String[] args) {
+    private static GraphTraversal<?, ?> getProjectTraversal(final GraphTraversalSource g, final int repeats,
+                                                            final String[] args) {
         GraphTraversal<?, ?> traversal = g.V().project(args[0], Arrays.copyOfRange(args, 1, repeats));
         for (int i = 0; i < repeats; i++) {
             traversal = traversal.by(__.valueMap(true));
@@ -67,16 +105,50 @@ public class PerformanceTest {
         return traversal;
     }
 
-    public static void executePerformanceTests() {
+    private static void executePerformanceTests() {
         System.out.println("~~~~~~~ PERFORMANCE TESTS STARTED ~~~~~~~");
-        System.out.println("~~~~~~~ RUNNING ONE ITEM PERFORMANCE TEST ~~~~~~~");
-        executeGetNextPerformanceTest();
-        System.out.println("~~~~~~~ RUNNING LIST PERFORMANCE TEST ~~~~~~~");
-        executeGetToListPerformanceTest();
+        // System.out.println("~~~~~~~ RUNNING ONE ITEM PERFORMANCE TEST ~~~~~~~");
+        // executeGetNextPerformanceTest();
+        // System.out.println("~~~~~~~ RUNNING LIST PERFORMANCE TEST ~~~~~~~");
+        // executeGetToListPerformanceTest();
+        System.out.println("~~~~~~~ RUNNING THROUGHPUT PERFORMANCE TEST ~~~~~~~");
+        POOL_SIZE.forEach(poolSize -> {
+            POOL_QUERY_COUNT.forEach(queryCount -> {
+                System.out.println("~~~ Pool size " + poolSize + ", query count " + queryCount + " ~~~");
+                executeThroughputPerformanceTest(poolSize, queryCount);
+            });
+        });
         System.out.println("~~~~~~~ PERFORMANCE TESTS COMPLETE ~~~~~~~");
     }
 
-    public static TimingData executeGetNextPerformanceTest() {
+    private static void executeThroughputPerformanceTest(final int poolSize, final int queryCount) {
+        final DriverRemoteConnection connection = getDriverRemoteConnection(poolSize);
+        final GraphTraversalSource g = getGraphTraversalSource(connection);
+        final Client client = getClient(getCluster(poolSize));
+        final List<Duration> durations = new ArrayList<>();
+        for (int i = 0; i < SAMPLE_SIZE; i++) {
+            try {
+                final List<CompletableFuture<ResultSet>> results = new ArrayList<>();
+                final Instant start = Instant.now();
+                for (int j = 0; j < queryCount; j++) {
+                    final String[] args = getArgs(POOLING_TRAVERSAL_SIZE);
+                    results.add(client.submitAsync(getProjectTraversal(g, POOLING_TRAVERSAL_SIZE, args).asAdmin().getBytecode()));
+                }
+                for (final CompletableFuture<ResultSet> result : results) {
+                    result.get().all().get();
+                }
+                final Instant end = Instant.now();
+                durations.add(Duration.between(start, end));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        final TimingData data = getTimingDataFromDurationList(durations);
+        System.out.println(data.toThroughput("Pooling", queryCount));
+    }
+
+    private static void executeGetNextPerformanceTest() {
+        GraphTraversalSource g = getGraphTraversalSource(getDriverRemoteConnection(null));
         final List<Duration> durations = new ArrayList<>();
         for (int i = 0; i < SAMPLE_SIZE; i++) {
             try {
@@ -90,12 +162,12 @@ public class PerformanceTest {
                 e.printStackTrace();
             }
         }
-        TimingData data = getTimingDataFromDurationList(durations);
+        final TimingData data = getTimingDataFromDurationList(durations);
         System.out.println(data.toStringMillis("One Item"));
-        return data;
     }
 
-    public static TimingData executeGetToListPerformanceTest() {
+    private static void executeGetToListPerformanceTest() {
+        final GraphTraversalSource g = getGraphTraversalSource(getDriverRemoteConnection(null));
         final List<Duration> durations = new ArrayList<>();
         for (int i = 0; i < SAMPLE_SIZE; i++) {
             try {
@@ -109,45 +181,26 @@ public class PerformanceTest {
                 e.printStackTrace();
             }
         }
-        TimingData data = getTimingDataFromDurationList(durations);
+        final TimingData data = getTimingDataFromDurationList(durations);
         System.out.println(data.toStringMillis("List"));
-        return data;
-    }
-
-    public static TimingData executeGetToListPerformanceTest() {
-        final List<Duration> durations = new ArrayList<>();
-        for (int i = 0; i < SAMPLE_SIZE; i++) {
-            try {
-                final String[] args = getArgs(VALUE_MAP_REPEATS);
-                final Instant starts = Instant.now();
-                // execute and retrieve(timed)
-                getProjectTraversal(g, VALUE_MAP_REPEATS, args).toList();
-                final Instant ends = Instant.now();
-                durations.add(Duration.between(starts, ends));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        TimingData data = getTimingDataFromDurationList(durations);
-        System.out.println(data.toStringMillis("List"));
-        return data;
     }
 
     private static TimingData getTimingDataFromDurationList(final List<Duration> durations) {
         Collections.sort(durations);
-        durations.remove(durations.size()-1);
+        durations.remove(durations.size() - 1);
         return new TimingData(
                 durations.stream().reduce(Duration.ZERO, Duration::plus).dividedBy(durations.size()),
                 durations.get(durations.size() / 2),
-                durations.get((int)(durations.size() * 0.90)),
-                durations.get((int)(durations.size() * 0.95)),
+                durations.get((int) (durations.size() * 0.90)),
+                durations.get((int) (durations.size() * 0.95)),
                 durations.get(0),
                 durations.get(durations.size() - 1)
         );
     }
 }
 
-private class TimingData {
+
+class TimingData {
     final Duration AVG;
     final Duration MEDIAN;
     final Duration PERCENTILE_90;
@@ -155,7 +208,8 @@ private class TimingData {
     final Duration MIN;
     final Duration MAX;
 
-    TimingData(Duration avg, Duration median, Duration percentile_90, Duration percentile_95, Duration min, Duration max) {
+    TimingData(Duration avg, Duration median, Duration percentile_90, Duration percentile_95, Duration min,
+               Duration max) {
         AVG = avg;
         MEDIAN = median;
         PERCENTILE_90 = percentile_90;
@@ -165,7 +219,7 @@ private class TimingData {
     }
 
     public String toStringMillis(final String testType) {
-        return "Test Type: " + testType + "\n"+
+        return "Test Type: " + testType + "\n" +
                 "\tAVG=" + AVG.toMillis() + "ms \n" +
                 "\tMEDIAN=" + MEDIAN.toMillis() + "ms \n" +
                 "\tPERCENTILE_90=" + PERCENTILE_90.toMillis() + "ms \n" +
@@ -174,13 +228,13 @@ private class TimingData {
                 "\tMAX=" + MAX.toMillis() + "ms \n";
     }
 
-    public String toStringNanos(final String testType) {
-        return "Test Type " + testType + "\n"+
-                "\tAVG=" + AVG.toNanos() + "ms \n" +
-                "\tMEDIAN=" + MEDIAN.toNanos() + "ms \n" +
-                "\tPERCENTILE_90=" + PERCENTILE_90.toNanos() + "ms \n" +
-                "\tPERCENTILE_95=" + PERCENTILE_95.toNanos() + "ms \n" +
-                "\tMIN=" + MIN.toNanos() + "ms \n" +
-                "\tMAX=" + MAX.toNanos() + "ms \n";
+    public String toThroughput(final String testType, int queryCount) {
+        return "Test Type: " + testType + "\n" +
+                "\tAVG=" + ((1000L * queryCount) / AVG.toMillis()) + " query/s \n" +
+                "\tMEDIAN=" + ((1000L * queryCount) / MEDIAN.toMillis()) + " query/s \n" +
+                "\tPERCENTILE_90=" + ((1000L * queryCount) / PERCENTILE_90.toMillis()) + " query/s \n" +
+                "\tPERCENTILE_95=" + ((1000L * queryCount) / PERCENTILE_95.toMillis()) + " query/s \n" +
+                "\tMIN=" + ((1000L * queryCount) / MIN.toMillis()) + " query/s \n" +
+                "\tMAX=" + ((1000L * queryCount) / MAX.toMillis()) + " query/s \n";
     }
 }
